2aadee8b070c9842e638cdb1d2f94db894d2d2b5
Israel Wong <israelwong@iMac-de-Israel.local>
2025-06-27 11:11:50 -0600

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-      <p className="mt-4 text-lg">
-        This is a simple Next.js application with Tailwind CSS.
+      <h1 className="text-4xl font-bold">Grupo Cultural Albatros</h1>
+      <p className="text-sm italic">
+        Portal en desarrollo.
       </p>
-      <Image
-        src="/nextjs-logo.png"
-        alt="Next.js Logo"
-        width={200}
-        height={200}
-        className="mt-8"
-      />
     </main>
   );
 }
